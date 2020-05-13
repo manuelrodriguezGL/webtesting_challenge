0caa4f6c21e8e59bd1fbd29c018015db96018d69
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2020-05-12 18:55:29 -0600

    Removed wait methods

---
+++ webtesting/src/main/java/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import selenium.BotStyle;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(tagName = "title")
+    private WebElement title;
+
+    public LoginPage(WebDriver driver)
+    {
+        super(driver);
+        super.initElements(driver, this);
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        if(!title.isDisplayed()){
+            throw new Error("Page not loaded!");
+        }
+    }
+}
