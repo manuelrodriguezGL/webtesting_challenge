ee66c747ac4b3c75af1d59192097286f25ee3e53
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2020-05-31 21:53:03 -0600

    A little cleanup of packages

--- webtesting/src/test/java/inventoryTests/InventoryTest.java
+++ webtesting/src/test/java/inventoryTests/InventoryTest.java
@@ -1,9 +1,12 @@
 package inventoryTests;
 
+import Constants.GlobalTestConstants;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import testBase.TestCaseBase;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InventoryTest extends TestCaseBase {
 
@@ -14,5 +17,10 @@ public class InventoryTest extends TestCaseBase {
         getLoginPage().login(user, pwd);
     }
 
-    //@Test
+    @Test(description = "Verify items on inventory page",
+            groups = {"UI"})
+    public void verifyInventoryUI() {
+        String errorMessages = getLoginPage().assesPageElements();
+        assertTrue(errorMessages.isEmpty(), GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE + errorMessages);
+    }
 }
