34040f64f8ef11683737240afeffeb16e8918f35
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2020-06-20 20:37:52 -0600

    Finished basic product tests

--- webtesting/src/test/java/productTests/ProductTest.java
+++ webtesting/src/test/java/productTests/ProductTest.java
@@ -1,6 +1,7 @@
 package productTests;
 
 import Constants.GlobalTestConstants;
+import dataProviders.InventoryDataProvider;
 import dataProviders.ProductsDataProvider;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -23,7 +24,7 @@ public class ProductTest extends TestCaseBase {
 
 
     @Test(description = "Verify the UI elements for every individual product",
-            groups = {"test"}, dataProvider = "Products", dataProviderClass = ProductsDataProvider.class)
+            groups = {"product"}, dataProvider = "Products", dataProviderClass = ProductsDataProvider.class)
     public void verifyProductUI(String id, String imageUrl, String name, String description, String price) {
         ProductPage product = inventoryPage.loadProductPageById(id);
         String errorMessages = "";
@@ -33,6 +34,54 @@ public class ProductTest extends TestCaseBase {
             e.printStackTrace();
         }
         assertTrue(errorMessages.isEmpty(), GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE + errorMessages);
+    }
+
+    @Test(description = "Verify that the Back button works on every product page",
+            groups = {"product"}, dataProvider = "ID", dataProviderClass = InventoryDataProvider.class)
+    public void verifyProductGoBackButton(String productId) {
+        ProductPage product = inventoryPage.loadProductPageById(productId);
+        boolean result = false;
+
+        try {
+            result = (product.goBack() != null);
+        } catch (Exception e) {
+            result = false;
+            e.printStackTrace();
+        }
+        assertTrue(result, GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE +
+                String.format("Could not go back on product with ID %s!", productId));
+    }
+
+    @Test(description = "Verify that every individual product can be added to cart",
+            groups = {"product"}, dataProvider = "ID", dataProviderClass = InventoryDataProvider.class)
+    public void verifyProductAddToCart(String productId) {
+        ProductPage product = inventoryPage.loadProductPageById(productId);
+        boolean result = false;
+
+        try {
+            result = product.clickAddToCartButton();
+        } catch (Exception e) {
+            result = false;
+            e.printStackTrace();
+        }
+        assertTrue(result, GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE +
+                String.format("Could not add product with ID %s to cart!", productId));
+    }
+
+    @Test(description = "Verify that every individual product can be removed from cart",
+            groups = {"product"}, dataProvider = "ID", dataProviderClass = InventoryDataProvider.class)
+    public void verifyProductRemoveFromCart(String productId) {
+        ProductPage product = inventoryPage.loadProductPageById(productId);
+        boolean result = false;
+
+        try {
+            result = (product.clickAddToCartButton() && product.clickRemoveButton());
+        } catch (Exception e) {
+            result = false;
+            e.printStackTrace();
+        }
+        assertTrue(result, GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE +
+                String.format("Could not remove product with ID %s from cart!", productId));
     }
 
 }
