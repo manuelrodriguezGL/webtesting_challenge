f48a0b9b83981136feb79241a57e932b13be8bb7
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2021-01-11 16:01:57 -0600

    Added tests for cart difference after removing one item

--- webtesting/src/test/java/shoppingCartTests/CheckoutOverviewTest.java
+++ webtesting/src/test/java/shoppingCartTests/CheckoutOverviewTest.java
@@ -61,7 +61,7 @@ public class CheckoutOverviewTest extends TestCaseBase {
     }
 
     @Test(description = "Verify the total values for the items on cart",
-            groups = {"checkoutOverview"})
+            groups = {"login"})
     public void verifyCartTotal() {
         boolean result = false;
         try {
@@ -72,5 +72,39 @@ public class CheckoutOverviewTest extends TestCaseBase {
 
         assertTrue(result, GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE +
                 "The following values don't match: Total and Item total plus Tax!");
+    }
+
+    @Test(description = "Verify the changes on cart total amount, by removing items",
+            groups = {"checkoutOverview"})
+    @Parameters({"item4ID", "item4Price", "customerFirstName", "customerLastName", "customerZipCode"})
+    public void verifyCartTotalChanges(String item4ID, String item4Price, String fName, String lName, String zip) {
+        boolean result = false;
+        double totalBefore = 0;
+        double totalAfter = 0;
+        try {
+            totalBefore = checkoutOverviewPage.getSubtotalAmount();
+
+            inventory = checkoutOverviewPage.cancelCheckout();
+            inventory.removeFromCartById(item4ID);
+            checkoutOverviewPage = reloadCartOverview(fName, lName, zip);
+
+            totalAfter = checkoutOverviewPage.getSubtotalAmount();
+
+            // A small trick to reduce decimal places without casting to String
+            double difference = Math.floor((totalBefore - totalAfter) * 100) / 100;
+            result = difference == Double.parseDouble(item4Price.substring(item4Price.indexOf("$") + 1));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(result, GlobalTestConstants.GLOBAL_TEST_FAILED_MESSAGE +
+                "The following values don't match: Total before and after removing one element");
+    }
+
+    private CheckoutOverviewPage reloadCartOverview(String fName, String lName, String zip) {
+        checkoutInformationPage = inventory.loadShoppingCart().clickCheckoutButton();
+        checkoutInformationPage.enterCustomerData(fName, lName, zip);
+        return checkoutInformationPage.clickContinue();
     }
 }
