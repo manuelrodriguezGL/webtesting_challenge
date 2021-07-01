f49cb52daaf04f1f0430a4831825e3486ecac38b
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2021-07-01 16:20:27 -0600

    Testing better listener with logging

--- src/main/java/listeners/TestExecutionListener.java
+++ src/main/java/listeners/TestExecutionListener.java
@@ -10,14 +10,19 @@ public class TestExecutionListener extends TestListenerAdapter {
     @Override
     public void onTestStart(ITestResult testResult) {
         super.onTestStart(testResult);
+        Reporter.log("Executing the following test: " + testResult.getName());
     }
 
     @Override
     public void onTestFailure(ITestResult testResult) {
-        //Reporter
-        ScreenshotUtil.takeScreenshot(testResult.getTestClass() + "_" + testResult.getName());
+        Reporter.log("The following test has failed: " + testResult.getName());
+        if (ScreenshotUtil.takeScreenshot(testResult.getTestClass() + "_" + testResult.getName())) {
+            Reporter.log("Screenshot has been created successfully");
+        }
     }
 
     @Override
-    public void onTestSuccess(ITestResult testResult) {}
+    public void onTestSuccess(ITestResult testResult) {
+        Reporter.log("The following test has passed: " + testResult.getName());
+    }
 }
