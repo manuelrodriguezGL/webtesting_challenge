f9218cb1f082ba524888f0cc2f4bc2e8037c12a7
manuelrodriguez <manuel.rodriguez@gorillalogic.com>
2021-12-23 10:53:35 -0600

    Removed some other static calls

--- src/test/java/testBase/TestCaseBase.java
+++ src/test/java/testBase/TestCaseBase.java
@@ -14,15 +14,14 @@ import selenium.SeleniumBase;
 @Listeners(TestExecutionListener.class)
 public class TestCaseBase extends SeleniumBase {
     protected final String GLOBAL_TEST_FAILED_MESSAGE = "Test execution failed! Message: \n";
-
-    private static LoginPage loginPage = null;
     protected String baseUrl = "";
+    private LoginPage loginPage = null;
 
     @BeforeMethod(alwaysRun = true)
     @Parameters({"browser", "headlessMode", "baseUrl"})
     public void setUp(String browserName, String headless, String _baseUrl, ITestContext context) {
 
-        WebDriver driver = super.setup(browserName, Boolean.valueOf(headless));
+        WebDriver driver = super.setup(browserName, Boolean.parseBoolean(headless));
 
         // Setting the webdriver attribute to the test context
         // This way, we avoid using static definitions of the web driver
@@ -38,14 +37,14 @@ public class TestCaseBase extends SeleniumBase {
         super.quit();
     }
 
-    public LoginPage getLoginPage() {
+    protected LoginPage getLoginPage() {
         if (loginPage == null) {
             loginPage = new LoginPage(getWebDriverInstance(), baseUrl);
         }
         return loginPage;
     }
 
-    public ProductsInventoryPage login(String user, String pwd) {
+    protected ProductsInventoryPage login(String user, String pwd) {
         return getLoginPage().login(user, pwd);
     }
 
